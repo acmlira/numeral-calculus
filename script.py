29183d89cff8c7b8cfe17e1e1e2a92d8327ae569
Allan C. M. Lira <acmlira@gmail.com>
2018-09-08 22:43:51 -0300

    Refactor first step :airplane:

--- script.py
+++ script.py
@@ -1,5 +1,15 @@
 from Bissection import bissection
 from FalsePosition import false_position
+import matplotlib.pyplot as plt
+import numpy as np
 
+# false_position(("(x**2) - 1"), 0.5, 1.5, 0.005, 20)
 
-false_position(("(x**2) - 1"), 0.5, 1.5, 0.005, 20)
+def f(function): 
+    return eval(function)
+
+x = np.arange(-10, 10, 0.00001)
+
+plt.plot(x ,f("- x - 1"), '-')
+plt.grid(True)
+plt.show()
