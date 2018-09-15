dc27c0a39598b92a30b73e8bf69436de3a2d93a8
Allan César <acmlira@gmail.com>
2018-09-15 13:53:41 -0300

    Refactor changes :triumph:

--- mpf.py
+++ mpf.py
@@ -7,24 +7,27 @@
 #   Algoritmo do Método Numérico: Método da Posição Fixa
 #
 #   Base environment: Anaconda 3 (Python 3.6)
-def mpf(fi, x0, E, iterMax=100):
-    if(abs(fi(x0)) < E):
-        return Exception
-    k = 1
-    print("x aprox\t\t\tf\t\t\tf'\t\t\tk")
-    while k < iterMax:
+def mpf(str_of_f, str_of_fi, x0, epsilon, iterMax=20):
+    f = lambda x: eval(str_of_f)
+    fi = lambda x: eval(str_of_fi)
+    
+    if abs(f(x0)) <= epsilon:           # Testa x0 para saber se já é a raiz desejada
+        return (False, x0)              
 
-        x1 = fi(x0)
+    print("--------------------------------------------------------------------------------------------------------------------")
+    print("k\tx1\t\tf(x1)\t\tfi(x1)")
+    print("--------------------------------------------------------------------------------------------------------------------")
+    print("-\t%e\t%e\t%e" % (x0, f(x0), fi(x0)))
+
+    k = 0
+    while k < iterMax:
         k += 1
-        
-        print("%e\t\t%e\t\t%e\t\t" % (x1,  fi(x1), k))
-        
-        if (abs(fi(x1)) < E):
-            break 
-            
+        x1 = fi(x0)
+        print("%i\t%e\t%e\t%e" % (k, x1, f(x1), fi(x1)))
+        if abs(f(x1)) <= epsilon:
+            return (True, x1)
         x0 = x1
-        
-        
-        if (k == iterMax):
-            return Exception
-    return x0
+    
+    return (False, x0)
+
+print(mpf("(x**2) + x - 6","(6 - x)**(1/2)",1.5,0.00000001))
--- newton_raphson.py
+++ newton_raphson.py
@@ -2,36 +2,34 @@
 #   Disciplina de Métodos Numéricos (2018.2 UFC)
 #
 #   Autor: Allan César
-#   Data: 12/09/2018
+#   Data: 24/08/2018
 #
-#   Algoritmo do Método Numérico: Secante
+#   Algoritmo do Método Numérico: Newton-Raphson
 #
 #   Base environment: Anaconda 3 (Python 3.6)
-def secant(str_of_f, x0, x1, epsilon, iterMax=100):
+def newton_raphson(str_of_f, str_of_flin, x0, epsilon, iterMax=20):  
     f = lambda x: eval(str_of_f)
-
+    flin = lambda x: eval(str_of_flin)
+    
+    
     if abs(f(x0)) <= epsilon:                   # Teste para saber se x0 já é a raiz (usa abs para conter os valores negativos)
         return (False, x0)                      # Retorna uma tupla com False (por que o método falhou) e com a raiz
-    if abs(f(x1)) <= epsilon:                   # Teste para saber se x0 já é a raiz (usa abs para conter os valores negativos)
-        return (False, x1)
-
-    x2 = x1 - (f(x1)/((f(x1) - f(x0)) / (x1 - x0)))
+    
     print("--------------------------------------------------------------------------------------------------------------------")
-    print("k\tx2\t\tf(x2)")
+    print("k\tx1\t\tf(x1)\t\tflin(x1)")
     print("--------------------------------------------------------------------------------------------------------------------")
-    print("-\t%e\t%e-" % (x2, f(x2)))
+    print("-\t%e\t%e\t%e\t-" % (x0, f(x0), flin(x0)))
     
     k = 0                                       # Inicia as iterações                                       
 
     while k < iterMax:                          # Loop condicionado
-
         k += 1
-        x2 = x1 - (f(x1)/((f(x1) - f(x0)) / (x1 - x0)))
-        print("%i\t%e\t%e" % (k, x2, f(x2)))
-        if abs(f(x2)) <= epsilon:
-            return (True, x2)
+        x1 = x0 - (f(x0)/flin(x0))
+        print("%i\t%e\t%e\t%e" % (k, x1, f(x1), flin(x1)))
+        if abs(f(x1)) <= epsilon:
+            return (True, x1)
         x0 = x1
-        x1 = x2
+        
     return (False, x0) 
 
-print(secant("(x**2) + x - 6",1.5,1.7,0.001))
+print(newton_raphson("(x**2) + x - 6","2*x + 1",1.5,0.001))
