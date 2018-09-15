cd65b1610e2b28964e47aaaeff97e0e78afd2034
Allan César <acmlira@gmail.com>
2018-09-15 16:11:26 -0300

    Adding a new method :rocket:

---
+++ newton_raphson_polynominal.py
@@ -0,0 +1,65 @@
+#
+#   Disciplina de Métodos Numéricos (2018.2 UFC)
+#
+#   Autor: Allan César
+#   Data: 24/08/2018
+#
+#   Algoritmo do Método Numérico: Newton-Raphson
+#
+#   Base environment: Anaconda 3 (Python 3.6)
+# def newton_raphson_polynominal(str_of_f, str_of_flin, x0, epsilon, iterMax=20):  
+#     f = lambda x: eval(str_of_f)
+#     flin = lambda x: eval(str_of_flin)
+    
+    
+#     if abs(f(x0)) <= epsilon:                   # Teste para saber se x0 já é a raiz (usa abs para conter os valores negativos)
+#         return (False, x0)                      # Retorna uma tupla com False (por que o método falhou) e com a raiz
+    
+#     print("--------------------------------------------------------------------------------------------------------------------")
+#     print("k\tx1\t\tf(x1)\t\tflin(x1)")
+#     print("--------------------------------------------------------------------------------------------------------------------")
+#     print("-\t%e\t%e\t%e\t-" % (x0, f(x0), flin(x0)))
+    
+#     k = 0                                       # Inicia as iterações                                       
+
+#     while k < iterMax:                          # Loop condicionado
+#         k += 1
+#         x1 = x0 - (f(x0)/flin(x0))
+#         print("%i\t%e\t%e\t%e" % (k, x1, f(x1), flin(x1)))
+#         if abs(f(x1)) <= epsilon:
+#             return (True, x1)
+#         x0 = x1
+        
+#     return (False, x0) 
+
+# print(newton_raphson_polynominal("(x**2) + x - 6","2*x + 1",1.5,0.001))
+def newton_raphson_polynominal(n, a, x, epsilon, iterMax=20):      
+    n = len(a)-1
+    print("k\t x\t\t p(x)")
+    k = 0
+    
+    while k <= iterMax:    
+        b = [1.0] * len(a)
+        c = [1.0] * n
+
+        i = n - 1
+        while i != 0:
+            b[i] = a[i] + b[i+1]*x
+            c[i-1] = b[i] + c[i]*x
+            i -= 1
+
+        b[0] = a[0] + b[1]*x
+   
+        print("%d\t %e\t %e\t" %(k, x, b[0]))
+
+        if abs(b[0]) <= epsilon:
+            return(False, x)
+        if abs(c[0]) == 0 :
+            print("Erro! c = 0, divisão por zero")
+            return (True, None)
+        
+        x = x - b[0]/c[0]
+        k += 1
+
+    print("Erro! Número máximo de iterações atingido")
+    return(True, x)
