65a58deae90bc85f13cf8512b3644bbe1aa78339
Allan C. M. Lira <acmlira@gmail.com>
2018-11-03 20:03:35 -0300

    Debugging

--- gauss_jordan.py
+++ gauss_jordan.py
@@ -16,7 +16,7 @@ def gauss_jordan(A, b, pivoted):
         for j in range (i+1, n):
             m = A[j][i]/A[i][i]
             for k in range(n):
-                I[j][k] = I[j][k] - m * A[i][k]
+                I[j][k] = I[j][k] - m * I[i][k]
                 A[j][k] = A[j][k] - m * A[i][k]
             b[j] = b[j] - m * b[i] 
 
@@ -24,26 +24,26 @@ def gauss_jordan(A, b, pivoted):
         for j in range (i-1, -1, -1):
             m = A[j][i]/A[i][i]
             for k in range(0, n):
-                I[j][k] = I[j][k] - m * A[i][k]
+                I[j][k] = I[j][k] - m * I[i][k]
                 A[j][k] = A[j][k] - m * A[i][k]
             b[j] = b[j] - m * b[i]
 
     for i in  range(0, n):
         for j in range(i, n):
             if i == j:
-                b[i] = round(b[i]/A[i][j], 2)
+                b[i] = b[i]/A[i][j]
                 for k in range(0, n):
-                    A[i][k] = A[i][k]/A[i][j]
                     I[i][k] = I[i][k]/A[i][j]
+                    A[i][k] = A[i][k]/A[i][j]
 
     return (A, b, det, p, I)
 
-A = [[  3, -4,  1],
-     [  1,  2,  2],
-     [  4,  0, -3]]
+A = [[  3,  2,  4],
+     [  1,  1,  2],
+     [  4,  3, -2]]
 
-b =  [  9,  3, -2]
+b =  [  1,  2,  3]
 
 (A, b, det, p, I) = gauss_jordan(A, b, False)
 
-print(A ,"\n\n", b, "\n\n", det)
+print(I ,"\n\n", A ,"\n\n", b, "\n\n", det)
