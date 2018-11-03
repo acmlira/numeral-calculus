17d4a60706adc7383b31b0fa196aeccaa92c1148
Allan C. M. Lira <acmlira@gmail.com>
2018-11-03 20:26:15 -0300

    Debugging 2

--- gauss_jordan.py
+++ gauss_jordan.py
@@ -13,28 +13,28 @@ def gauss_jordan(A, b, pivoted):
 
         det *= A[i][i]
 
-        for j in range (i+1, n):
-            m = A[j][i]/A[i][i]
-            for k in range(n):
-                I[j][k] = I[j][k] - m * I[i][k]
-                A[j][k] = A[j][k] - m * A[i][k]
-            b[j] = b[j] - m * b[i] 
-
-    for i in range(n-1, -1, -1):
-        for j in range (i-1, -1, -1):
+        for j in range (0, n):
             m = A[j][i]/A[i][i]
             for k in range(0, n):
-                I[j][k] = I[j][k] - m * I[i][k]
-                A[j][k] = A[j][k] - m * A[i][k]
-            b[j] = b[j] - m * b[i]
+                if j != i:
+                    I[j][k] = I[j][k] - m * I[i][k]
+                    A[j][k] = A[j][k] - m * A[i][k]
+            b[j] = b[j] - m * b[i] 
+
+    # for i in range(n-1, -1, -1):
+    #     for j in range (n-1, -1, -1):
+    #         m = A[j][i]/A[i][i]
+    #         for k in range(0, n):
+    #             I[j][k] = I[j][k] - m * I[i][k]
+    #             A[j][k] = A[j][k] - m * A[i][k]
+    #         b[j] = b[j] - m * b[i]
 
     for i in  range(0, n):
-        for j in range(i, n):
-            if i == j:
-                b[i] = b[i]/A[i][j]
-                for k in range(0, n):
-                    I[i][k] = I[i][k]/A[i][j]
-                    A[i][k] = A[i][k]/A[i][j]
+        b[i] = b[i]/A[i][i]
+        for j in range(0, n):
+            I[i][j] = I[i][j]/A[i][i]
+            A[i][j] = A[i][j]/A[i][i]
+
 
     return (A, b, det, p, I)
 
